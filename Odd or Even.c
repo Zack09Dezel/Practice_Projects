3f80f4b96b91ab73159edb331c7c3acb3e176cf4
Gusti Michael Ferdinand <149660909+Zack09Dezel@users.noreply.github.com>
2024-05-21 22:37:14 +0800

    v0.3

---
+++ Odd or Even.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int x, choices;
+
+    printf("\n---------------------------");
+    printf("\nOdd and Even Number Checker");
+    printf("\n---------------------------\n\n");
+    
+    do{
+        printf("Enter the number you want to check: ");
+        scanf("%i", &x);
+
+        if(x % 2 == 0){ 
+            printf("\n%i is an Even number\n", x);
+        }else{	
+            printf("\n%i is an Odd number\n", x);
+        }
+            
+        while(1){
+            printf("\nAgain?\n");
+            printf("1. Yes\n2. No\n");
+            scanf("%d", &choices);
+            if(choices==1 || choices==2){
+                break;
+            }else{
+                printf("\nThat was incorrect, please choose between continuing or not");
+            }
+        }    
+    }while(choices==1);
+    return 0;
+}
